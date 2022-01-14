6d4a1b60719ca2c8b6e56f4a6630536c48720be2
Marc Meier <xernerac@gmail.com>
2022-01-14 17:58:55 +0100

    Finished the Matrix part - Reformated the code a bit - Renamed the struct from `matrix` to `Matrix` - Added array-like accessing of elements - Added vector dot and cross products

--- Matrix.h
+++ Matrix.h
@@ -5,73 +5,72 @@
 #ifndef MOZ_MATRIX_H
 #define MOZ_MATRIX_H
 
-
+#define MOZ_MATRIX__INCLUDE_COMPONENT_BINDINGS  // If this is defined, matrix components can be accessed with matrix.x, matrix.y, matrix.z, matrix.w
 
 template <typename type, int cols, int rows>
-struct matrix {
+struct Matrix {
+
+#ifdef MOZ_MATRIX__INCLUDE_COMPONENT_BINDINGS
 	union {
 		type cells[rows*cols];
-		struct {type x; type y; type z; type w;};
-		struct {type r; type g; type b; type a;};
-		struct {type s; type t; type u; type v;};
-	}; // God I love the unsafe!
+		struct { type x; type y; type z; type w; };
+		struct { type r; type g; type b; type a; };
+		struct { type s; type t; type u; type v; };
+	}; // God, I love the unsafe!
+#else
+	type cells[rows*cols];
+#endif
 
+	Matrix() = default;
 
-	matrix() = default;
-
-	explicit matrix(type fill) {
-		for (int i = 0; i < rows*cols; i++) cells[i] = fill;
-	}
+	explicit Matrix(type fill) { for (int i = 0; i < rows*cols; i++) cells[i] = fill; }
 
 	template<typename... types>
-	explicit matrix(type first, types... values) {
+	explicit Matrix(type first, types... values) {
 		static_assert(sizeof...(values)+1 == rows*cols, "Not the correct number of arguments passed for matrix initialization");
 		type newCells[rows*cols] = {first, values...};
 		for (int i = 0; i < rows*cols; i++) cells[i] = newCells[i];
 	}
 
-	inline type get_cell(int cell_x, int cell_y) {
-		return cells[cell_x*rows + cell_y];
-	}
-	inline void set_cell(int cell_x, int cell_y, type value) {
-		cells[cell_x*rows + cell_y] = value;
-	}
+	// TODO: This could use static_asserts
+	inline type get_cell(int cell_x, int cell_y)      { return cells[cell_x*rows + cell_y]; }
+	inline void set_cell(int cell_x, int cell_y, type value) { cells[cell_x*rows + cell_y] = value; }
 
 	template <int other_cols>
-	inline matrix<type, other_cols, rows> operator*(matrix<type, other_cols, cols> other) {
-		matrix<type, other_cols, rows> result;
+	inline Matrix<type, other_cols, rows> operator*(Matrix<type, other_cols, cols> other) {
+		Matrix<type, other_cols, rows> result;
 
 		for (int cell_x = 0; cell_x < other_cols; cell_x++) { for (int cell_y = 0; cell_y < rows; cell_y++) {
-				type total = get_cell(0, cell_y) * other.get_cell(cell_x, 0);
-				for (int r = 1; r < cols; r++) {
-					type a = get_cell(r, cell_y);
-					type b = other.get_cell(cell_x, r);
-					total = total + a*b;
-				}
-				result.set_cell(cell_x, cell_y, total);
-			}}
+			type total = get_cell(0, cell_y) * other.get_cell(cell_x, 0);
+			for (int r = 1; r < cols; r++) {
+				type a = get_cell(r, cell_y);
+				type b = other.get_cell(cell_x, r);
+				total = total + a*b;
+			}
+			result.set_cell(cell_x, cell_y, total);
+		}}
 
 		return result;
 	}
 
-	inline matrix<type, cols, rows> operator*(type other) {
-		matrix<type, cols, rows> result;
+	inline Matrix<type, cols, rows> operator*(type other) {
+		Matrix<type, cols, rows> result;
 		for (int i = 0; i < cols*rows; i++) {
 			result.cells[i] = cells[i]*other;
 		}
 		return result;
 	}
 
-	inline matrix<type, cols, rows> operator+(matrix<type, cols, rows> other) {
-		matrix<type, cols, rows> result;
+	inline Matrix<type, cols, rows> operator+(Matrix<type, cols, rows> other) {
+		Matrix<type, cols, rows> result;
 		for (int i = 0; i < cols*rows; i++) {
 			result.cells[i] = other.cells[i]+cells[i];
 		}
 		return result;
 	}
 
-	inline matrix<type, cols, rows> operator-(matrix<type, cols, rows> other) {
-		matrix<type, cols, rows> result;
+	inline Matrix<type, cols, rows> operator-(Matrix<type, cols, rows> other) {
+		Matrix<type, cols, rows> result;
 		for (int i = 0; i < cols*rows; i++) {
 			result.cells[i] = other.cells[i]-cells[i];
 		}
@@ -85,18 +84,36 @@ struct matrix {
 		}
 	}
 
-	bool operator==(matrix<type, cols, rows> other) {
+	inline bool operator==(Matrix<type, cols, rows> other) {
 		for (int i = 0; i < cols*rows; i++) {
 			if (cells[i] != other.cells[i]) return false;
 		}
 		return true;
 	}
+
+	inline type  operator[](unsigned i) const { return cells[i]; }
+	inline type& operator[](unsigned i)       { return cells[i]; }
+
+	inline Matrix<type, rows, cols> transpose() {
+		Matrix<type, rows, cols> result;
+		for (int cell_x = 0; cell_x < cols; cell_x++) { for (int cell_y = 0; cell_y < rows; cell_y++) {
+			result.set_cell(cell_y, cell_x, get_cell(cell_x, cell_y));
+		}}
+		return result;
+	}
+
+	inline Matrix<type, rows, cols> T() { return transpose(); }
+
 };
 
 
+template <typename type, int components> using Vector = Matrix<type,  1, components>;
 
-template <int cols, int rows> using fmat = matrix<float,  cols, rows>;
-template <int cols, int rows> using dmat = matrix<double, cols, rows>;
+template <int cols, int rows> using fmat = Matrix<float,  cols, rows>;
+template <int cols, int rows> using dmat = Matrix<double, cols, rows>;
+
+template <int components> using fvec = Vector<float,  components>;
+template <int components> using dvec = Vector<double, components>;
 
 using fmat2 = fmat<2, 2>;
 using fmat3 = fmat<3, 3>;
@@ -106,9 +123,6 @@ using dmat2 = dmat<2, 2>;
 using dmat3 = dmat<3, 3>;
 using dmat4 = dmat<4, 4>;
 
-template <int components> using fvec = matrix<float,  1, components>;
-template <int components> using dvec = matrix<double, 1, components>;
-
 using fvec2 = fvec<2>;
 using fvec3 = fvec<3>;
 using fvec4 = fvec<4>;
@@ -116,5 +130,24 @@ using fvec4 = fvec<4>;
 using dvec2 = dvec<2>;
 using dvec3 = dvec<3>;
 using dvec4 = dvec<4>;
+
+
+
+
+
+
+template<typename type, int components>
+inline type dot(Vector<type, components> a, Vector<type, components> b) {
+	return (a.T() * b)[0];
+}
+
+template<typename type>
+inline Vector<type, 3> cross(Vector<type, 3> a, Vector<type, 3> b) {
+	return Vector<type, 3>(a.y*b.z - a.z*b.y,
+						   a.z*b.x - a.x*b.z,
+						   a.x*b.y - a.y*b.x);
+}
+
+
 
 #endif //MOZ_MATRIX_H
--- testing.cpp
+++ testing.cpp
@@ -4,19 +4,18 @@
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include "library.h"
 #include "Matrix.h"
 
 
 
-TEST_CASE("Matrix initialization", "[matrix][init]") {
-	matrix<float, 2, 2> a;
+TEST_CASE("Matrix initialization", "[matrix]") {
+	Matrix<float, 2, 2> a;
 	a.set_cell(0, 0, 6.f);
 	a.set_cell(0, 1, 9.f);
 	a.set_cell(1, 0, 2.f);
 	a.set_cell(1, 1, 3.f);
 
-	matrix<float, 2, 2> b = matrix<float, 2, 2>(6.f, 9.f, 2.f, 3.f);
+	Matrix<float, 2, 2> b = Matrix<float, 2, 2>(6.f, 9.f, 2.f, 3.f);
 	bool same1 = a == b;
 
 	REQUIRE(same1);
@@ -26,17 +25,17 @@ TEST_CASE("Matrix initialization", "[matrix][init]") {
 	a.set_cell(1, 0, 18.f);
 	a.set_cell(1, 1, 18.f);
 
-	b = matrix<float, 2, 2>(18.f);
+	b = Matrix<float, 2, 2>(18.f);
 	bool same2 = a == b;
 	REQUIRE(same2);
 
 
 }
 
-TEST_CASE("Matrix equality check", "[matrix][equality]") {
-	matrix<float, 2, 2> a = matrix<float, 2, 2>(1.f, 2.f, 3.f, 4.f);
-	matrix<float, 2, 2> b = matrix<float, 2, 2>(1.f, 2.f, 3.f, 4.f);
-	matrix<float, 2, 2> c = matrix<float, 2, 2>(23.f, 34.f, 31.f, 46.f);
+TEST_CASE("Matrix equality check", "[matrix]") {
+	Matrix<float, 2, 2> a = Matrix<float, 2, 2>(1.f, 2.f, 3.f, 4.f);
+	Matrix<float, 2, 2> b = Matrix<float, 2, 2>(1.f, 2.f, 3.f, 4.f);
+	Matrix<float, 2, 2> c = Matrix<float, 2, 2>(23.f, 34.f, 31.f, 46.f);
 
 	bool a_is_a = a == a;
 	bool a_is_b = a == b;
@@ -51,10 +50,10 @@ TEST_CASE("Matrix equality check", "[matrix][equality]") {
 }
 
 
-TEST_CASE("Check for correct matrix multiplication", "[matrix][multiplication]") {
-	matrix<float, 2, 2> a = matrix<float, 2, 2>(1.f, 2.f, 3.f, 4.f);
-	matrix<float, 2, 2> b = matrix<float, 2, 2>(5.f, 6.f, 7.f, 8.f);
-	matrix<float, 2, 2> c = matrix<float, 2, 2>(23.f, 34.f, 31.f, 46.f);
+TEST_CASE("Check for correct matrix multiplication", "[matrix]") {
+	Matrix<float, 2, 2> a = Matrix<float, 2, 2>(1.f, 2.f, 3.f, 4.f);
+	Matrix<float, 2, 2> b = Matrix<float, 2, 2>(5.f, 6.f, 7.f, 8.f);
+	Matrix<float, 2, 2> c = Matrix<float, 2, 2>(23.f, 34.f, 31.f, 46.f);
 
 	bool t = c == a*b;
 	REQUIRE(t);
@@ -62,7 +61,7 @@ TEST_CASE("Check for correct matrix multiplication", "[matrix][multiplication]")
 }
 
 
-TEST_CASE("Vector Components", "[matrix][vector][components]") {
+TEST_CASE("Vector Components", "[matrix][vector]") {
 	fvec4 v = fvec4(1.f, 3.f, 2.f, 7.f);
 
 	REQUIRE(v.x == v.get_cell(0, 0));
@@ -84,4 +83,45 @@ TEST_CASE("Vector Components", "[matrix][vector][components]") {
 	REQUIRE(v.y == v.get_cell(0, 1));
 	REQUIRE(v.z == v.get_cell(0, 2));
 	REQUIRE(v.w == v.get_cell(0, 3));
+}
+
+
+TEST_CASE("Array-like accessing", "[matrix][vector]") {
+	fvec4 v = fvec4(3.f, 5.f, 4.f, 8.f);
+
+	REQUIRE(v.x == v[0]);
+	REQUIRE(v.y == v[1]);
+	REQUIRE(v.z == v[2]);
+	REQUIRE(v.w == v[3]);
+
+	fvec4 n = fvec4(3.f, 5.f, 4.f, 8.f);
+
+	v[0] = n.x;
+	v[1] = n.y;
+	v[2] = n.z;
+	v[3] = n.w;
+
+	REQUIRE(v.x == n.x);
+	REQUIRE(v.y == n.y);
+	REQUIRE(v.z == n.z);
+	REQUIRE(v.w == n.w);
+}
+
+TEST_CASE("Matrix Transpose", "[matrix]") {
+	fmat3 orig = fmat3(1.f, 2.f, 3.f,
+					   4.f, 5.f, 6.f,
+					   7.f, 8.f, 9.f);
+
+	fmat3 tran = orig.transpose();
+
+	fmat3 reco = tran.T();
+
+	fmat3 expe = fmat3(1.f, 4.f, 7.f,
+					   2.f, 5.f, 8.f,
+					   3.f, 6.f, 9.f);
+
+	bool is_symmetric = reco == orig;
+	bool is_correct  = tran == expe;
+	REQUIRE(is_symmetric);
+	REQUIRE(is_correct);
 }
